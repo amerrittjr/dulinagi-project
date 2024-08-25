52dd4e15bd5a2f4092fa3f2d52438b6142f94e5c
Dre Jr <andredmerritt@gmail.com>
2024-08-24 23:38:55 -0400

    build 1.0.0

---
+++ script.js
@@ -0,0 +1,44 @@
+// video player
+
+document.addEventListener("DOMContentLoaded", function () {
+  const videos = document.querySelectorAll(".background-video");
+  let currentVideoIndex = 0;
+
+  function showNextVideo() {
+    videos[currentVideoIndex].classList.remove("active");
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    videos[currentVideoIndex].classList.add("active");
+  }
+
+  videos[currentVideoIndex].classList.add("active");
+
+  setInterval(showNextVideo, 10000);
+});
+
+//modal
+
+function closeAllModals() {
+  document.querySelectorAll(".modal.visible").forEach((modal) => {
+    modal.classList.remove("visible");
+  });
+}
+
+document.querySelectorAll("[data-modal]").forEach((item) => {
+  item.addEventListener("click", function () {
+    closeAllModals();
+    const modalId = this.getAttribute("data-modal");
+    document.getElementById(modalId).classList.add("visible");
+  });
+});
+
+document.querySelectorAll(".close").forEach((closeBtn) => {
+  closeBtn.addEventListener("click", function () {
+    this.closest(".modal").classList.remove("visible");
+  });
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target.classList.contains("modal")) {
+    event.target.classList.remove("visible");
+  }
+});
